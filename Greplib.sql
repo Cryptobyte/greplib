fb90915241e68f21b4999cc31f99cf51f6d9a139
Crypt.NoIP <quantumadolf@gmail.com>
2014-09-26 14:07:55 -0400

    Island Database Update
    Added ocean number to island list in database, removed not needed
    function GetOceanNumberFromTown($ID) from Greplib.Functions

--- Greplib.sql
+++ Greplib.sql
@@ -1,31 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 25, 2014 at 05:47 PM
--- Server version: 10.0.10-MariaDB-1~precise-log
--- PHP Version: 5.3.10-1ubuntu3.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `spyda_grep`
---
+CREATE DATABASE IF NOT EXISTS `spyda_grep` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `spyda_grep`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Alliances`
---
-
+DROP TABLE IF EXISTS `Alliances`;
 CREATE TABLE IF NOT EXISTS `Alliances` (
   `id` int(15) NOT NULL,
   `name` varchar(30) NOT NULL,
@@ -36,12 +20,7 @@ CREATE TABLE IF NOT EXISTS `Alliances` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Conquers`
---
-
+DROP TABLE IF EXISTS `Conquers`;
 CREATE TABLE IF NOT EXISTS `Conquers` (
   `town_id` int(15) NOT NULL,
   `time` int(11) NOT NULL,
@@ -53,12 +32,7 @@ CREATE TABLE IF NOT EXISTS `Conquers` (
   PRIMARY KEY (`town_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Ghosts`
---
-
+DROP TABLE IF EXISTS `Ghosts`;
 CREATE TABLE IF NOT EXISTS `Ghosts` (
   `id` int(15) NOT NULL,
   `name` varchar(30) NOT NULL,
@@ -70,16 +44,12 @@ CREATE TABLE IF NOT EXISTS `Ghosts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Islands`
---
-
+DROP TABLE IF EXISTS `Islands`;
 CREATE TABLE IF NOT EXISTS `Islands` (
   `id` int(15) NOT NULL,
   `x` int(5) NOT NULL,
   `y` int(5) NOT NULL,
+  `ocean` int(3) NOT NULL,
   `island_type` int(5) NOT NULL,
   `available_towns` int(5) NOT NULL,
   `res_plus` varchar(10) NOT NULL,
@@ -87,12 +57,7 @@ CREATE TABLE IF NOT EXISTS `Islands` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Players`
---
-
+DROP TABLE IF EXISTS `Players`;
 CREATE TABLE IF NOT EXISTS `Players` (
   `id` int(15) NOT NULL,
   `name` varchar(30) NOT NULL,
@@ -103,12 +68,7 @@ CREATE TABLE IF NOT EXISTS `Players` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Towns`
---
-
+DROP TABLE IF EXISTS `Towns`;
 CREATE TABLE IF NOT EXISTS `Towns` (
   `id` int(15) NOT NULL,
   `player_id` int(15) NOT NULL,
